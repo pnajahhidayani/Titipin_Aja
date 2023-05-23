55f89c44337fea0885d167efffb4d537c2000b9e
Putri Najah Hidayani <pnajahhidayanidex@gmail.com>
2023-05-23 18:32:47 +0700

    bikin layout login

--- app/src/main/java/org/d3if3029/titipinaja/MainActivity.kt
+++ app/src/main/java/org/d3if3029/titipinaja/MainActivity.kt
@@ -1,12 +1,11 @@
 package org.d3if3029.titipinaja
 
-import android.os.Build.VERSION_CODES.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.Layout.activity_main)
+        setContentView(R.layout.activity_main)
     }
 }
