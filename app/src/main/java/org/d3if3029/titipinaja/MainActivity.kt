0ee22a83573fa4019dd208e36c1f6c0b101346b0
p_najahhidayani <pnajahhidayanidex@gmail.com>
2023-05-29 22:03:37 +0700

    week-3

--- app/src/main/java/org/d3if3029/titipinaja/MainActivity.kt
+++ app/src/main/java/org/d3if3029/titipinaja/MainActivity.kt
@@ -2,10 +2,48 @@ package org.d3if3029.titipinaja
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.TextView
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+
+    private val onNavigationItemSelectedListener = ButtomNavigationView.OnNavigationItemSelectedListener { item->
+        when (item.itemId) {
+            R.id.nav_home -> {
+                textView.setText("Home")
+                return@OnNavigationItemSelectedListener
+                true
+            }
+            R.id.nav_chat -> {
+                textView.setText("Chat")
+                return@OnNavigationItemSelectedListener
+                true
+            }
+            R.id.nav_add_post -> {
+                textView.setText("Add Post")
+                return@OnNavigationItemSelectedListener
+                true
+            }
+            R.id.nav_notifications -> {
+                textView.setText("Notifications")
+                return@OnNavigationItemSelectedListener
+                true
+            }
+//            R.id.nav_profile -> {
+//                textView.setText("Profile")
+//                return@OnNavigationItemSelectedListener
+//                true
+//            }
+        }
+        false
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navView: ButtomNavigationView = findViewById(R.id.nav_view)
+
+        textView = findViewById(R.id.message)
+
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
 }
