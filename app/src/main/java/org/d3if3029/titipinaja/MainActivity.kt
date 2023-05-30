354c547b7dd4527502ffaf1d3cdf19cd4ba7b44c
p_najahhidayani <pnajahhidayanidex@gmail.com>
2023-05-30 22:10:41 +0700

    Buat fragment (masih ada yang belum di delete dari tutor sebelumnya) buttom navigationnya masih merah

--- app/src/main/java/org/d3if3029/titipinaja/MainActivity.kt
+++ app/src/main/java/org/d3if3029/titipinaja/MainActivity.kt
@@ -2,48 +2,58 @@ package org.d3if3029.titipinaja
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.fragment.app.Fragment
+import org.d3if3029.titipinaja.fragments.HomeFragment
+import org.d3if3029.titipinaja.fragments.ProfileFragment
+import org.d3if3029.titipinaja.fragments.SearchFragment
+
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textView: TextView
+    internal var selectedFragment: Fragment? = null
 
     private val onNavigationItemSelectedListener = ButtomNavigationView.OnNavigationItemSelectedListener { item->
         when (item.itemId) {
+
             R.id.nav_home -> {
-                textView.setText("Home")
-                return@OnNavigationItemSelectedListener
-                true
+               selectedFragment = HomeFragment()
             }
             R.id.nav_chat -> {
-                textView.setText("Chat")
-                return@OnNavigationItemSelectedListener
-                true
+                selectedFragment = SearchFragment()
+            }
+            R.id.nav_search -> {
+                selectedFragment = ChatFragment()
             }
             R.id.nav_add_post -> {
-                textView.setText("Add Post")
                 return@OnNavigationItemSelectedListener
                 true
             }
-            R.id.nav_notifications -> {
-                textView.setText("Notifications")
-                return@OnNavigationItemSelectedListener
-                true
-            }
-//            R.id.nav_profile -> {
-//                textView.setText("Profile")
+//            R.id.nav_notifications -> {
 //                return@OnNavigationItemSelectedListener
 //                true
 //            }
+            R.id.nav_profile -> {
+                selectedFragment = ProfileFragment()
+            }
+        }
+        if(selectedFragment != null) {
+            supportFragmentManager.beginTransaction().replace(
+                R.id.fragment_container,
+                selectedFragment!!
+            ).commit()
         }
         false
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val navView: ButtomNavigationView = findViewById(R.id.nav_view)
 
-        textView = findViewById(R.id.message)
-
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        supportFragmentManager.beginTransaction().replace(
+            R.id.fragment_container,
+            HomeFragment()
+        ).commit()
     }
 }
